8cab7075fdc79609bb37fa9bf32eac505035d499
James Heath <jh230155@falmouth.ac.uk>
2020-02-28 18:31:15 +0000

    Added a Movement Script Added a Camera Follow Script.

---
+++ Assets/Git-Pushed To The Limit/Movement/CameraFollow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Camera Position")]
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offset;
+
+    [Header("Camera Boundaries")]
+    [SerializeField]
+    private float min = 0;
+    [SerializeField]
+    private float max = 0;
+
+    void Start()
+    {
+        offset = this.transform.position - target.position;
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            this.transform.position = offset + target.position;
+
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Git-Pushed To The Limit/Movement/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private bool jumped = false;
+    [SerializeField]
+    private Vector2 jumpForce;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveHorizontal * speed * Time.deltaTime, 0.0f);
+
+        if ((Input.GetKeyDown(KeyCode.W)) && (!jumped))
+        {
+            jumped = true;
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if ((other.gameObject.tag == "Ground") || (other.gameObject.tag == "Platform"))
+        {
+            jumped = false;
+        }
+    }
+}
