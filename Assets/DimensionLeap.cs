7fd575700c3cdc4c85e8e8f8aa91be44e2fdbce1
Billy-Burton <56894266+Billy-Burton@users.noreply.github.com>
2020-02-28 18:27:34 +0000

    Added the dimensional leap

---
+++ Assets/DimensionLeap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DimensionLeap : MonoBehaviour
+{ 
+    public GameObject VoidPlatforms;
+    public GameObject RealPlatforms;
+    // Start is called before the first frame updat
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Leap();
+        }
+
+    }
+
+    public void Leap()
+    {
+        if (VoidPlatforms.activeSelf == true)
+        {
+            VoidPlatforms.SetActive(false);
+            RealPlatforms.SetActive(true);
+        }
+        else if (RealPlatforms.activeSelf == true)
+        {
+            VoidPlatforms.SetActive(true);
+            RealPlatforms.SetActive(false);
+        }
+    }
+}
