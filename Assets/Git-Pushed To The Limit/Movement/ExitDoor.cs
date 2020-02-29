73c0f627f3fdd6490e58746b16323ddf266f6afd
James Heath <jh230155@falmouth.ac.uk>
2020-02-29 14:04:42 +0000

    Player colliding with the door will load the next scene.

--- Assets/Git-Pushed To The Limit/Movement/ExitDoor.cs
+++ Assets/Git-Pushed To The Limit/Movement/ExitDoor.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitDoor : MonoBehaviour
 {
 
-    void OnCollisionEnter2D()
+    void OnCollisionEnter2D(Collision other)
     {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
 
+        }
     }
 }
