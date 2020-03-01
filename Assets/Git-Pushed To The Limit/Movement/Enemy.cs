f8cf2db98a7bd75a27a766219a9cd76305c37463
James Heath <jh230155@falmouth.ac.uk>
2020-03-01 15:00:10 +0000

    Enemies will now change how they look based on if they are in the same reality as the player. They now swap between two different sprites.

--- Assets/Git-Pushed To The Limit/Movement/Enemy.cs
+++ Assets/Git-Pushed To The Limit/Movement/Enemy.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Sprite realitySprite;
-    public Sprite voidSprite;
+    public Sprite sameDimension;
+    public Sprite otherDimension;
 
     [Header("Enemy Stats")]
     [SerializeField]
@@ -17,7 +17,7 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private bool stun = false;
     public bool canMove = true;
-    [Tooltip("Enemy Current Dimension: True = Reality, False = Void")]
+    public bool realityDimension;
 
     public DimensionLeap DimensionLeap;
 
@@ -45,6 +45,16 @@ public class Enemy : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (realityDimension == DimensionLeap.dimension)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = sameDimension;
+        }
+
+        else if (realityDimension != DimensionLeap.dimension)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = otherDimension;
+        }
+
         Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
         Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
         isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
--- Assets/Git-Pushed To The Limit/Movement/HostileVoidling.cs
+++ Assets/Git-Pushed To The Limit/Movement/HostileVoidling.cs
@@ -23,7 +23,7 @@ public class HostileVoidling : Enemy
     IEnumerator Charge()
     {
         yield return new WaitForSecondsRealtime(2);
-        voidling.GetComponent<SpriteRenderer>().sprite = voidling.GetComponent<HostileVoidling>().realitySprite;
+        voidling.GetComponent<Enemy>().realityDimension = true;
         voidling.gameObject.layer = 9;
         Debug.Log("Reality");
         StartCoroutine(Cooldown());
@@ -32,7 +32,7 @@ public class HostileVoidling : Enemy
     IEnumerator Cooldown()
     {
         yield return new WaitForSecondsRealtime(2);
-        voidling.GetComponent<SpriteRenderer>().sprite = voidling.GetComponent<HostileVoidling>().voidSprite;
+        voidling.GetComponent<Enemy>().realityDimension = false;
         voidling.gameObject.layer = 10;
         move.canMove = true;
         Debug.Log("Void");
