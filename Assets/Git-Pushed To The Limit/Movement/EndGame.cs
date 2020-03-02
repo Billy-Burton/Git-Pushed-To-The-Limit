2a5169f980f11d87d5dfb94ba2ca5dd33e915956
James Heath <jh230155@falmouth.ac.uk>
2020-03-02 10:45:29 +0000

    You can now enter a door on the main level that takes you back to the main menu.

---
+++ Assets/Git-Pushed To The Limit/Movement/EndGame.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndGame : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+
+        }
+    }
+}
